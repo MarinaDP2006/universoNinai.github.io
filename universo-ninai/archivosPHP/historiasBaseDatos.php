1fedb59d7d831cf65fb17c038d141b3da5d46545
MarinaDP2006 <marinadompas@gmail.com>
2025-11-14 13:13:39 +0100

    Add new story entry to historias array

--- universo-ninai/archivosPHP/historiasBaseDatos.php
+++ universo-ninai/archivosPHP/historiasBaseDatos.php
@@ -7,9 +7,9 @@
 $historias = array(
   array('id' => '1','seccion' => 'ultimas','titulo' => 'Infinite Game'),
   array('id' => '2','seccion' => 'ultimas','titulo' => 'Entre páginas e historias'),
+  array ('id' => '3','seccion' => 'ultimas','titulo' => 'Tom & Elaine: Desde que te conocí.'),
   array('id' => '4','seccion' => 'top','titulo' => 'La luz del Rey'),
   array('id' => '5','seccion' => 'top','titulo' => 'Dayan Denali'),
   array('id' => '6','seccion' => 'top','titulo' => 'El gorrión y la mujer del mar'),
-  array('id' => '7','seccion' => 'recomendadas','titulo' => 'Entre páginas e historias')
-);
+  array('id' => '7','seccion' => 'recomendadas','titulo' => 'Entre páginas e historias'));
 ?>
